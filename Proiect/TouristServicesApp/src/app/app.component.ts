2007e88fac39196ad17367a22e573f8ee9ffc114
MalihinMihai <malihin.mihai@gmail.com>
2020-11-18 19:06:34 +0200

    Actualizari : design pagina login, design left-menu, pregatire implementare interfata harti

---
+++ Proiect/TouristServicesApp/src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Platform, MenuController } from '@ionic/angular';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Router, RouterEvent, NavigationEnd } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss']
+})
+export class AppComponent implements OnInit {
+    public selectedIndex = 0;
+    public appPages = [
+   
+    {
+      title: 'About Us',
+      url: '/about',
+      icon: 'paper-plane'
+    },
+    {
+      title: 'Rate US',
+      url: '/folder/RateUS',
+      icon: 'star'
+    },
+    {
+      title: 'Login',
+      url: '/login',
+      icon: 'heart'
+    },
+    {
+      title: 'Exit',
+      url: '/folder/Exit',
+      icon: 'archive'
+    },
+  
+  ];
+  
+
+  constructor(
+    private platform: Platform,
+    private splashScreen: SplashScreen,
+      private statusBar: StatusBar,
+     //Variabila pentru setarea paginii de login ca root.
+      private router: Router,
+      
+      
+  ) {
+      this.initializeApp();
+     
+  }
+
+  initializeApp() {
+    this.platform.ready().then(() => {
+    this.statusBar.styleDefault();
+    this.splashScreen.hide();
+    //Setare pagina login ca Root.
+    this.router.navigateByUrl('login');
+    });
+  }
+
+  ngOnInit() {
+    const path = window.location.pathname.split('folder/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.appPages.findIndex(page => page.title.toLowerCase() === path.toLowerCase());
+    }
+     
+  }
+}
