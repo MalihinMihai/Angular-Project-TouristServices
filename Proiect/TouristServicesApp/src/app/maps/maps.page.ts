c1e7e289f1e889425879c54837a5a3c50f02f8d7
MalihinMihai <malihin.mihai@gmail.com>
2020-11-27 16:57:44 +0200

    Google maps implementat

--- Proiect/TouristServicesApp/src/app/maps/maps.page.ts
+++ Proiect/TouristServicesApp/src/app/maps/maps.page.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {ViewChild, ElementRef } from '@angular/core';
+
+
+declare var google: any;
+
 
 @Component({
   selector: 'app-maps',
@@ -7,7 +12,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MapsPage implements OnInit {
 
-  constructor() { }
+  map:any;
+  @ViewChild('map',{read: ElementRef, static: false}) mapRef: ElementRef;
+
+
+   constructor() {}
+   ionViewDidEnter(){
+   this.showMap();
+    }
+
+    showMap(){
+    const location = new google.maps.LatLng(45.648129, 25.569335);
+    const options = {
+    center: location,
+    zoom: 15,
+    disableDefaultUI: true
+}
+this.map = new google.maps.Map(this.mapRef.nativeElement,options);
+}
+
 
   ngOnInit() {
   }
